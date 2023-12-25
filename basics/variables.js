b09c8d5878a350777b42772049c6cf580d03702c
Lakshyajeet-solanki11 <78341853+Lakshyajeet-solanki11@users.noreply.github.com>
2023-12-25 08:51:11 +0000

    commit

---
+++ basics/variables.js
@@ -0,0 +1,17 @@
+const accountId = 14454
+let accountEmail = "lakshyajeet@google.com"
+var accountPassword = "123445"
+accountCity = "jaipur"
+
+    // accountId = 15555 // not allowed 
+
+    accountEmail = "hc@hc.com"
+    accountPassword = "21212121"
+    accountCity = "bengaluru"
+
+
+console.log(accountId);
+console.table([accountEmail, accountPassword, accountCity]);
+
+
+
